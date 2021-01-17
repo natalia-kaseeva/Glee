6752fb2fc1cb9a35184aa9237b2dd87b35bf728d
Natalia <nat.kaseeva@gmail.com>
2021-01-17 21:37:00 +0400

    pixel perfect changes

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,6 @@ $(function(){
     $('.slider__inner').slick({
         dots:true,
         arrows:false
-        // customPaging : function(slider, i) {
-        //     var thumb = $(slider.$slides[i]).data();
-        //     return '<a>'+i+'</a>';
-        //     or
-        //     return '<a>'+(i+1)+'</a>';
-        //     },
     });
 
     var mixer = mixitup('.product__cards');
