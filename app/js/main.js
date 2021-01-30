0b2c418620528f4d9413044e186feb0c0cdebc47
Natalia <nat.kaseeva@gmail.com>
2021-01-30 23:23:48 +0400

    media queries were added

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,29 @@ $(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1000,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              }
+        ]
     });
 
     var containerEl1 = $('[data-ref="container-1"]');
